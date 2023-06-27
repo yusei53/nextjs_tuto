71ce0b43ad148dcf3bb9c3a7e9cd79a611f69d4a
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-06-27 12:13:36 +0900

    変更してみた

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,6 @@ import Image from 'next/image'
 
 export default function Home() {
   return (
-    <div className="mt-20 text-4xl">TEST</div>
+    <div className="mt-20 text-4xl">TEST変更テスト</div>
   )
 }
