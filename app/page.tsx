bcd64b5620e7b8015bdf1018ab2221fe1c75da58
今村悠聖 <yusei@imamurayuushounoMacBook-Air.local>
2023-06-27 18:27:05 +0900

    コンポーネントのテスト

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
 import Image from 'next/image'
+import { Counter } from '@/components/counter'
 
 export default function Home() {
   return (
-    <div className="mt-20 text-4xl">TEST変更テスト</div>
-  )
+    <>
+      <div className="mt-20 text-4xl">TEST変更テスト</div>
+      <Counter></Counter>
+    </>
+  );
 }
