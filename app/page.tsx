460c765e440503601bdda0af4f1a4b799121f328
yusei53 <yusei530922@gmail.com>
2023-06-30 01:29:32 +0900

    退避

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,15 @@
-import Image from 'next/image'
+
 import { Counter } from '@/components/counter'
+import Image from "../public/img/icon.jpg";
 
 export default function Home() {
   return (
     <>
-      <div className="bg-current h-28">
-        <div className="text-black text-4xl font-inter font-bold">Liscript</div>
+      <div className="bg-red-100 h-28 flex items-center">
+        <img src="/img/icon.jpg" alt="" className='w-4'/>
+        <div className="text-black text-4xl font-extrabold ml-28">
+          Liscript
+        </div>
       </div>
     </>
   );
