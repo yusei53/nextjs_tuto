b6549fab0bf2c4fab6aa98bd5cd8255080ad3b32
yusei53 <yusei530922@gmail.com>
2023-06-30 16:20:48 +0900

    chaklaとemotionのinstall

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
-import Image from 'next/image'
-import { Counter } from '@/components/counter'
+import Image from "next/image";
+import { Counter } from "@/components/counter";
 
 export default function Home() {
   return (
     <>
-      <div className="mt-20 text-4xl">First Post</div>
-      <Counter></Counter>
+      <div>First Post</div>
     </>
   );
 }
